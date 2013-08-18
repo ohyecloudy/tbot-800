dd87a6e353bd9158aaddafd8ca71c02ea0c33187
ohyecloudy <ohyecloudy@gmail.com>
2013-08-18 18:38:13 +0900

    #2 트윗 스케쥴러 > config에 정의된 간격으로 트윗 * tweet-interval-ms key value로 정의 * rein run 으로 시작할 수 있게 :main 값 정의

--- _config.clj
+++ _config.clj
@@ -1,5 +1,5 @@
 {:app-consumer-key "FIXME"
  :app-consumer-secret "FIXME"
  :user-access-token "FIXME"
- :user-access-token-secret "FIXME"}
-
+ :user-access-token-secret "FIXME"
+ :tweet-interval-ms 1}
--- src/tbot_800/core.clj
+++ src/tbot_800/core.clj
@@ -1,9 +1,11 @@
 (ns tbot-800.core
   (:use
-    [twitter.oauth]
-    [twitter.callbacks]
-    [twitter.callbacks.handlers]
-    [twitter.api.restful]))
+   [twitter.oauth]
+   [twitter.callbacks]
+   [twitter.callbacks.handlers]
+   [twitter.api.restful])
+  (:import 
+   (java.util TimerTask Timer)))
 
 (def my-creds 
   (let [config (load-file "config.clj")]
@@ -12,12 +14,30 @@
                       (config :user-access-token)
                       (config :user-access-token-secret))))
 
+(def tweet-time-interval
+  (:tweet-interval-ms (load-file "config.clj")))
+
 (def quotes
   (filter (fn [q] (let [twit-length-limit 140]
                     (<= (count q) twit-length-limit)))
           (load-file "resources/quotes.clj")))
 
-(defn tweet-random-quote []
-  (statuses-update :oauth-creds my-creds
-                   :params {:status (rand-nth quotes)}))
+(defn tweet [msg]
+  (do
+   (println "tweet - " msg)
+   (try
+     (statuses-update :oauth-creds my-creds
+                    :params {:status msg})
+     (catch Exception e
+       (println "caught exception: "
+                (.getMessage e))))))
 
+(defn register-schedule-tweet [interval]
+  (let [task (proxy [TimerTask] []
+               (run []
+                 (tweet (rand-nth quotes))))
+        delay (long 1000)]
+    (. (new Timer) (schedule task delay (long interval)))))
+
+(defn -main [& args]
+  (register-schedule-tweet tweet-time-interval))
