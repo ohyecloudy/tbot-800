4f5f863772c425464725679394fd64260513b7b7
ohyecloudy <ohyecloudy@gmail.com>
2014-01-21 00:43:51 +0900

    twitter-api 0.7.5 버전업 > json 파싱 버그 픽스

--- tbot/project.clj
+++ tbot/project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [twitter-api "0.7.4"]
+                 [twitter-api "0.7.5"]
                  [clj-time "0.6.0"]]
   :main tbot.core)
