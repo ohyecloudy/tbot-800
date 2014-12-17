1b303a144553f4878ed4ee71737d8924e1f1990c
ohyecloudy <ohyecloudy@gmail.com>
2014-12-17 22:37:36 +0900

    store remain quotes in memory #20 #19
    * slurp 함수 사용해서 url에서 파일 읽는 건 줏어 먹었다.

--- _config.clj
+++ _config.clj
@@ -2,6 +2,5 @@
   :app-consumer-secret "FIXME"
   :user-access-token "FIXME"
   :user-access-token-secret "FIXME"
-  :quotes-path "FIXME"
-  :remain-path "FIXME"
+  :quotes-url "FIXME"
   :master-twitter-id "FIXME"}]
--- src/tbot-800/core.clj
+++ src/tbot-800/core.clj
@@ -13,23 +13,21 @@
                     (config :user-access-token)
                     (config :user-access-token-secret)))
 
-(defn load-remain-quote-repo [remain-path]
-  (if (.exists (as-file remain-path))
-    (read-string (slurp remain-path))
-    nil))
-
-(defn load-src-quote-repo [quotes-path master-id]
-  (let [shuffled-quotes (shuffle (load-file quotes-path))
+(defn build-quotes [master-id url]
+  (let [shuffled-quotes (shuffle (read-string (slurp url)))
         c (count shuffled-quotes)]
     (into [(str "인용구 트윗 한 바퀴 돕니다. 총 인용구는 " c "개입니다. @" master-id)]
           shuffled-quotes)))
 
-(defn quote-repo [quotes-path remain-path master-id]
-  (let [remain-repo (load-remain-quote-repo remain-path)
-        src-repo-loader #(load-src-quote-repo quotes-path master-id)]
-    (if (and (not (nil? remain-repo)) (< 0 (count remain-repo)))
-      remain-repo
-      (src-repo-loader))))
+(def quotes (ref {}))
+(defn pop-quote [builder url]
+  (let [k (keyword url)]
+    (do
+      (when (empty? (k @quotes))
+        (dosync (ref-set quotes (assoc @quotes k (builder url)))))
+      (let [q (first (k @quotes))]
+        (dosync (ref-set quotes (assoc @quotes k (rest (k @quotes)))))
+        q))))
 
 (defn tweet [creds msg]
   (try
@@ -38,19 +36,15 @@
     (catch Exception e
       (println "caught exception: " (.getMessage e)))))
 
-(defn tweet-then-prepare-next [config]
-  (let [src-path (:quotes-path config)
-        remain-path (:remain-path config)
-        master-id (:master-twitter-id config)
-        repo (quote-repo src-path remain-path master-id)]
-    (do
-      (tweet (make-creds config) (first repo))
-      (spit remain-path (prn-str (rest repo))))))
+(defn tweet-quote [config]
+  (let [master-id (:master-twitter-id config)
+        src-url (:quotes-url config)
+        quote-builder (partial build-quotes master-id)]
+    (tweet (make-creds config) (pop-quote quote-builder src-url))))
 
 (defn -main [& args]
   (if (not= 1 (count args))
     (println "need config file path ex) ./config.clj")
     (let [configs (load-file (first args))]
       (doall
-       (map tweet-then-prepare-next configs)))))
-
+       (map tweet-quote configs)))))
